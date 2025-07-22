df2be67257a70d47c1928995d91422305bd11143
Tarun <sdtarun2@gmail.com>
2025-07-22 17:57:33 +0000

    top rated movies should have atleast 10 votes and arrange according to r

--- src/lib/movie-service.ts
+++ src/lib/movie-service.ts
@@ -44,10 +44,10 @@ export const getMoviesByCategory = async (categoryId: string): Promise<Movie[]> 
     
     switch(categoryId) {
         case 'popular':
-            query = query.gte('vote_count', 10).order('vote_average', { ascending: false, nullsFirst: false });
+            query = query.order('vote_count', { ascending: false, nullsFirst: false });
             break;
         case 'top_rated':
-            query = query.order('vote_average', { ascending: false, nullsFirst: false });
+            query = query.gte('vote_count', 10).order('vote_average', { ascending: false, nullsFirst: false });
             break;
         case 'upcoming':
             query = query.filter('release_date', 'gt', new Date().toISOString()).order('release_date', { ascending: true });
