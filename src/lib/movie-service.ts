41f3aec9d82da423355980b28afbdb0dc3198915
Tarun <sdtarun2@gmail.com>
2025-07-22 05:58:35 +0000

    put the movies in popular with atleast 10 vote_count and then arrange th

--- src/lib/movie-service.ts
+++ src/lib/movie-service.ts
@@ -44,7 +44,7 @@ export const getMoviesByCategory = async (categoryId: string): Promise<Movie[]> 
     
     switch(categoryId) {
         case 'popular':
-            query = query.order('vote_average', { ascending: false, nullsFirst: false });
+            query = query.gte('vote_count', 10).order('vote_average', { ascending: false, nullsFirst: false });
             break;
         case 'top_rated':
             query = query.order('vote_average', { ascending: false, nullsFirst: false });
