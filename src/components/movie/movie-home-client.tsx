777626f3e58f4ae1cabbd529a15f31ad2c246243
Nishanth <nishanthvlr@gmail.com>
2025-07-25 06:33:43 +0000

    Push the filter sectioin, little up in the home page, as there is space

--- src/components/movie/movie-home-client.tsx
+++ src/components/movie/movie-home-client.tsx
@@ -239,7 +239,7 @@ export function MovieHomeClient({
     
     return (
         <>
-            <div className="mb-12 bg-neutral-800 border border-border rounded-2xl p-4 md:p-6">
+            <div className="mb-8 bg-neutral-800 border border-border rounded-2xl p-4">
                  {hasActiveFilters && (
                     <div className="flex justify-end mb-4 -mt-2 -mr-2">
                         <Button variant="ghost" onClick={handleClearFilters}>
